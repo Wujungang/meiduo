7cadad6920e2bfb89e612cb8b869a3a1314a872a
wujg <wujungang2011@126.com>
2019-06-03 10:47:29 +0800

    用户名，手机号后端验证实现

--- front_end_pc/js/register.js
+++ front_end_pc/js/register.js
@@ -63,6 +63,22 @@ var vm = new Vue({
             } else {
                 this.error_name = false;
             }
+            if (this.error_name == false){
+                axios.get(this.host + '/usernames/'+ this.username + '/count',{
+                    responseType: 'json'
+                })
+                    .then(response =>{
+                        if (response.data.count > 0){
+                            this.error_name_message = '用户名已存在';
+                            this.error_name = true;
+                        }else {
+                            this.error_name = false;
+                        }
+                    })
+                    .catch(error =>{
+                        console.log(error.response.data);
+                    })
+            }
 
         },
         check_pwd: function (){
@@ -87,6 +103,22 @@ var vm = new Vue({
             } else {
                 this.error_phone_message = '您输入的手机号格式不正确';
                 this.error_phone = true;
+            }
+            if (this.error_phone == false){
+                axios.get(this.host + '/mobiles/' +this.mobile + '/count/',{
+                    responseType:'json'
+                })
+                    .then(response =>{
+                        if (response.data.count >0){
+                            this.error_phone_message = '手机号已存在';
+                            this.error_phone = true;
+                        }else {
+                            this.error_phone = false;
+                        }
+                    })
+                    .catch(error =>{
+                        console.log(error.response.data);
+                    })
             }
 
         },
@@ -173,6 +205,34 @@ var vm = new Vue({
             this.check_sms_code();
             this.check_allow();
 
+            if(this.error_name == false && this.error_password == false && this.error_check_password == false
+                && this.error_phone == false && this.error_sms_code == false && this.error_allow == false) {
+                axios.post(this.host + '/users/', {
+                        username: this.username,
+                        password: this.password,
+                        password2: this.password2,
+                        mobile: this.mobile,
+                        sms_code: this.sms_code,
+                        allow: this.allow.toString()
+                    }, {
+                        responseType: 'json'
+                    })
+                    .then(response => {
+                        location.href = '/index.html';
+                    })
+                    .catch(error=> {
+                        if (error.response.status == 400) {
+                            if ('non_field_errors' in error.response.data) {
+                                this.error_sms_code_message = error.response.data.non_field_errors[0];
+                            } else {
+                                this.error_sms_code_message = '数据有误';
+                            }
+                            this.error_sms_code = true;
+                        } else {
+                            console.log(error.response.data);
+                        }
+                    })
+            }
 
         }
     }
--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
-
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from rest_framework.serializers import Serializer
+from rest_framework.views import APIView
+from users.models import User
+from . import serializers
 # Create your views here.
+
+
+class UserView(CreateAPIView):
+    serializer_class = serializers.CreateUserSerializer
+
+
+
+class UsernameCountView(APIView):
+    def get(self,request,username):
+        count = User.objects.filter(username=username).count()
+        data = {
+            'username':username,
+            'count':count
+        }
+        return Response(data)
+
+class MobileCountView(APIView):
+    def get(self,request,mobile):
+        count = User.objects.filter(mobile=mobile).count()
+        data = {
+            'mobile':mobile,
+            'count':count,
+        }
+        return Response(data)
