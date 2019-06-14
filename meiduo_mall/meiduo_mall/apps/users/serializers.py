5bde0c2bcc77756088bebe64364b65d99ac05be2
wujg <wujungang2011@126.com>
2019-06-14 15:35:10 +0800

    admin站点更新自动生成商品详情信息

--- meiduo_mall/meiduo_mall/apps/users/serializers.py
+++ meiduo_mall/meiduo_mall/apps/users/serializers.py
@@ -49,14 +49,14 @@ class EmailSerializer(serializers.ModelSerializer):
         instance.save()
         to_email = validated_data['email']
         verify_url = instance.generate_verify_email_url()
-        # send_active_email.delay(email,verify_url)
+        send_active_email.delay(to_email,verify_url)
 
-        subject = "美多商城邮箱验证"
-        html_message = '<p>尊敬的用户您好！</p>' \
-                       '<p>感谢您使用美多商城。</p>' \
-                       '<p>您的邮箱为：%s 。请点击此链接激活您的邮箱：</p>' \
-                       '<p><a href="%s">%s<a></p>' % (to_email, verify_url, verify_url)
-        send_mail(subject, "", settings.EMAIL_FROM, [to_email], html_message=html_message)
+        # subject = "美多商城邮箱验证"
+        # html_message = '<p>尊敬的用户您好！</p>' \
+        #                '<p>感谢您使用美多商城。</p>' \
+        #                '<p>您的邮箱为：%s 。请点击此链接激活您的邮箱：</p>' \
+        #                '<p><a href="%s">%s<a></p>' % (to_email, verify_url, verify_url)
+        # send_mail(subject, "", settings.EMAIL_FROM, [to_email], html_message=html_message)
 
         return instance
 
