e2dc3c52b90912f5f01aab582134d0f2e267355e
wujg <wujungang2011@126.com>
2019-06-23 22:53:29 +0800

    商品列表页展示

--- meiduo_mall/meiduo_mall/apps/goods/views.py
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from rest_framework.filters import OrderingFilter
-# Create your views here.
 from rest_framework.generics import ListAPIView
-
+from goods.models import SKU
+from . import serializers
 
 class SKUListView(ListAPIView):
-    pass
+    serializer_class = serializers.SKUSerializer
+    filter_backends = [OrderingFilter]
+    ordering_fields = ('create_time','price','sales')
+
+    def get_queryset(self):
+        category_id = self.kwargs['category_id']
+        print(category_id)
+        return SKU.objects.filter(category_id=category_id, is_launched=True)
