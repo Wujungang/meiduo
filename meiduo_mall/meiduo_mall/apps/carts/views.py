41aa7267d64a0160867c416a1170eb4c715f5604
wujg <wujungang2011@126.com>
2019-06-26 15:37:26 +0800

    修改购物车数据前后端成功

--- meiduo_mall/meiduo_mall/apps/carts/views.py
+++ meiduo_mall/meiduo_mall/apps/carts/views.py
@@ -14,6 +14,62 @@ from . import serializers
 class CartView(APIView):
     def perform_authentication(self, request):
         pass
+
+    def put(self,request):
+        serializer =serializers.CatSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        sku_id = serializer.validated_data.get('sku_id')
+        count = serializer.validated_data.get('count')
+        selected = serializer.validated_data.get('selected')
+        try:
+            user = request.user
+        except Exception:
+            user = None
+        if user is not None and user.is_authenticated:
+            #用户已登录在redis中保存
+            redis_conn = get_redis_connection('cart')
+            #创建reids管道
+            pl = redis_conn.pipeline()
+            #将商品的数量修改为传递过来的参数的数量
+            pl.hset('cart_%s'%user.id,sku_id,count)
+            #判断商品的勾选状态
+            if selected:
+                #若勾选的话，将商品的数量添加到勾选的列表中
+                pl.sadd('cart_selected_%s'%user.id,sku_id)
+                #不勾选的话则在商品的勾选列表中，删除商品id
+            else:
+                pl.srem('cart_selected_%s'%user.id,sku_id)
+            #执行管道
+            pl.execute()
+            #返回响应对象
+            return Response(serializer.data)
+        #如果用户没有登录
+        else:
+        #从cookies中获取购物车信息
+            cart = request.COOKIES.get('cart')
+        #判断购物车信息是否存在
+            if cart:
+            #转码获取购物车信息
+                cart = pickle.loads(base64.b64decode(cart.encode()))
+        #若购物车不存在，则创建空的字典
+            else:
+                cart = {}
+        #创建购物车数据
+            cart[sku_id] = {
+                'count':count,
+                'selected':selected
+            }
+            print(cart)
+        #将购物车数据转化成要往cookies中存储的数据
+            cart = base64.b64encode(pickle.dumps(cart)).decode('utf-8')
+        #创建相应对象
+            response = Response(serializer.data)
+        #将cookie写入到cookie中
+            response.set_cookie('cart',cart,max_age=60*60*60)
+
+        #返回相应对象
+        return response
+
     def get(self,request):
         #判断用户是否登录
         try:
